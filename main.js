7dfd0a8cb9aff2523b64b1caf3a7e3947744c1bd
HWestra17 <HarlanWestra@amdg.rockhursths.edu>
2018-12-11 11:08:46 -0600

    Update

--- main.js
+++ main.js
@@ -1,14 +1,72 @@
+var fox;
+var feed = [];
+var numFood = 10;
+
+var a = [1,2,3];
+
 function setup() {
-    createCanvas(640, 480);
+    rectMode(CENTER);
+    createCanvas(1024, 768);
+    fox = new Fox();
+
+    for (var i = 0; i < numFood; i++) {
+        feed.push(new Food(random(width), random(height)));
+    }
 }
 
 function draw() {
-   var x = width/2;
-    var y = height/2;
-    background('#FFFAED');
+    background(0, 255, 0);
+    fox.display();
+
+    for (var i = 0; i < feed.length; i++) {
+        feed[i].display();
+    }
 }
 
+function mousePressed() {
+    fox.eat();
+}
+
+function Food(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = color(255, 0, 0);
+    this.foodSize = 50;
+
+    this.display = function () {
+        fill(this.color);
+        ellipse(this.x, this.y, this.foodSize, this.foodSize);
+    }
+}
+
+function Fox() {
+    var x = mouseX;
+    var y = mouseY;
+    var diameter = 200;
+
+    this.getDistance = function (other) {
+        var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+        return dist;
+    };
+
+    this.eat = function () {
+        for (var i = feed.length - 1; i >= 0; i--) {
+            var food = feed[i];
+            var d = this.getDistance(food);
+            var r1 = food.foodSize / 2;
+            var r2 = diameter / 2;
+            if (r1 + r2 > d) {
+                feed.splice(i, 1);
+                feed.push(new Food(random(width), random(height)));
+            }
+        }
+    };
+
     this.display = function() {
+        x = mouseX;
+        y = mouseY;
+        noStroke();
+        
     //earLeft;
     fill('#FF7F50');
     push();
@@ -17,8 +75,8 @@ function draw() {
     ellipse(0,0, 190, 50);
     pop();
     
-    //earRight;
-    fill('#FF7F50');
+     //earRight;
+   fill('#FF7F50');
     push();
     translate(x+90, y-80);
     rotate(-Math.PI/2);
@@ -63,4 +121,4 @@ function draw() {
     arc(x-20, y+80, 40, 70, 0, PI+QUARTER_PI, CHORD);
     fill('#EE3E36');
     arc(x+10, y+80, 40, 70, 0, PI+QUARTER_PI, -CHORD);
-};
+}
